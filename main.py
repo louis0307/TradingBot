69f2f3ef659d8572c879ac105244a227e7fbd411
siegelou <siegelou@students.zhaw.ch>
2024-11-23 21:53:58 +0100

    v2.0-Trading

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import schedule
 from trading.calc_signal import trade_signal
+from data.stream_data import stream_data
+import schedule
 
 def main():
+    stream_data()
+
     times = ["00:10", "15:10", "30:10", "45:10"]
     for t in times:
         schedule.every().hour.at(t).do(trade_signal)
