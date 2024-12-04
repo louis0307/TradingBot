b296dbbab2c09be8e236acf77b7cff5295b89cb3
siegelou <siegelou@students.zhaw.ch>
2024-12-04 21:03:40 +0100

    v2.3-Trading

--- data/db_connection.py
+++ data/db_connection.py
@@ -9,4 +9,4 @@ DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 
-stream = sqlalchemy.create_engine(f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}', connect_args={'timeout': 20})
+stream = sqlalchemy.create_engine(f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}')
