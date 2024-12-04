ea088bdd7ced5e16c4f7e4353389533f0774f9ec
siegelou <siegelou@students.zhaw.ch>
2024-12-04 22:46:05 +0100

    v2.3-Trading

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def update_graph(selected_asset, n_intervals):
     return figure
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
