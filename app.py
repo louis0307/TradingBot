ce51b4a848c3d96b553e3b2df6e19ac657bd156c
siegelou <siegelou@students.zhaw.ch>
2025-02-10 20:38:33 +0100

    v2.5-Trading

--- app.py
+++ app.py
@@ -36,40 +36,17 @@ server = app.server
 dash_authentication = BasicAuth(server, '/login', '/dashboard', secret_key)
 
 server.secret_key = secret_key
-login_manager = LoginManager()
-login_manager.init_app(server)
-login_manager.login_view = 'login'
+if not server.secret_key:
+    raise ValueError("No SECRET_KEY set for Flask application.")
+@server.before_request
+def enforce_https():
+    if not request.is_secure and request.headers.get("X-Forwarded-Proto", "http") != "https": # X-Forwarded-Proto is there to deal with the fact that in Render reverse proxy/load balancer is used
+                                                                                              # and that nevertheless we can see whether the request is http or https
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code = 301)
 
-# User model
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
 users = {username: password}
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
-
-@app.route('/login', methods=['POST'])
-def login():
-    usr = request.form['username']
-    pwd = request.form['password']
-    if username == usr and password == pwd:
-        user = User(usr)
-        login_user(user)
-        return redirect('/dashboard')
-    return 'Invalid credentials', 401
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect('/login')
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return 'Welcome to the dashboard!'
+auth = dash_auth.BasicAuth(app, users)
 
 # Load complete history for each asset at startup
 data_dict = {}
@@ -86,22 +63,7 @@ for asset in ASSET_LIST:
         logger.error(f"Error loading data for {asset}: {e}")
         data_dict[asset] = pd.DataFrame({'Date': [], 'Price': []})
 
-
-#login_layout = dbc.Container([
-#    dbc.Row([
-#        dbc.Col(html.H2("Please Log In"), className="text-center mb-4")
-#    ]),
-#    dbc.Row([
-#        dbc.Col([
-#            dcc.Input(id='username', type='text', placeholder='Username'),
-#            dcc.Input(id='password', type='password', placeholder='Password'),
-#            html.Button('Login', id='login-button', n_clicks=0),
-#            html.Div(id='login-output', style={'color': 'red'})
-#        ], width=12)
-#    ])
-#])
-
-dashboard_layout = dbc.Container([
+app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H1("Trading Bot Dashboard"), className="text-center mb-4")
     ]),
@@ -127,11 +89,6 @@ dashboard_layout = dbc.Container([
                          )
         ])
     ]),
-#    dbc.Row([
-#        dbc.Col([
-#            html.Button('Logout', id='logout-button', n_clicks=0)
-#        ])
-#    ]),
     dbc.Row([
         dbc.Col([
             dcc.Interval(
@@ -143,78 +100,16 @@ dashboard_layout = dbc.Container([
     ])
 ])
 
-app.layout = dbc.Container([
-    dcc.Location(id='url', refresh=True),
-    html.Div(id='page-content'),
-#    dcc.Input(id='username', type='text', placeholder='Username', style={'display': 'none'}),
-#    dcc.Input(id='password', type='password', placeholder='Password', style={'display': 'none'}),
-#    html.Button('Login', id='login-button', n_clicks=0, style={'display': 'none'}),
-#    html.Button('Logout', id='logout-button', n_clicks=0, style={'display': 'none'}),
-    html.Button('Start Tradingbot', id='start-bot-button', n_clicks=0, style={'display': 'none'}),
-    html.Button('Stop Tradingbot', id='stop-bot-button', n_clicks=0, style={'display': 'none'}),
-    #html.Div(id='login-output', style={'display': 'none'}),
-    #dcc.Textarea(id='log-textarea', value='', style={'width': '100%', 'height': 200, 'display': 'none'}, readOnly=True)
-])
-
 @app.callback(
-    [Output('page-content', 'children'),
-#     Output('login-output', 'children'),
+    [Output('price-chart', 'figure'),
      Output('log-textarea', 'value', allow_duplicate=True)],
     [Input('url', 'pathname'),
-#     Input('login-button', 'n_clicks'),
-#     Input('logout-button', 'n_clicks'),
+     Input('asset-dropdown', 'value'),
+     Input('interval-component', 'n_intervals'),
      Input('start-bot-button', 'n_clicks'),
      Input('stop-bot-button', 'n_clicks')]
 )
-#def display_page(pathname, login_clicks, logout_clicks, start_bot_clicks , username, password, stop_bot_clicks, log_value):
-    # global bot_thread, bot_running
-#    ctx = dash.callback_context
-#    trigger = ctx.triggered[0]['prop_id'].split('.')[0] if ctx.triggered else None
 
-    # Initialize content and message
-    #content = login_layout
-    #message = ''
-    #log_update = log_value
-
-    # Determine which button was clicked
-    #if not ctx.triggered:
-    #    raise PreventUpdate
-
-    #trigger = ctx.triggered[0]['prop_id'].split('.')[0]
-
-#    if trigger == 'login-button' and login_clicks > 0:
-#        print(f"Login attempt with username: {username} and password: {password}")
-#        if username in users and users[username] == password:
-#            user = User(username)
-#            login_user(user)
-#            print("Login successful")
-#            content = dashboard_layout
-#            message = dcc.Location(pathname='/', id='redirect')
-#        else:
-#            print("Login failed")
-#            message = 'Invalid username or password'
-
-#    elif trigger == 'logout-button' and logout_clicks > 0:
-#        logout_user()
-#        print("Logout successful")
-#        content = login_layout
-#        log_update += '\nUser logged out.'
-
-#    if current_user.is_authenticated and (pathname == '/' or pathname == '/login'):
-#        print("User is authenticated, showing dashboard")
-#        content = dashboard_layout
-#    else:
-#        if pathname == '/':
-#            content = login_layout
-#    print(f"Content: {content}, Message: {message}")
-#    return content, message, log_update
-
-# Callback to update the chart based on selected asset
-@app.callback(
-    Output('price-chart', 'figure'),
-    [Input('asset-dropdown', 'value'),
-     Input('interval-component', 'n_intervals')]
-)
 def update_graph(selected_asset, n_intervals):
     try:
         query = f'SELECT * FROM public."{selected_asset}"'
@@ -248,11 +143,6 @@ def update_graph(selected_asset, n_intervals):
         }
     return figure
 
-@app.callback(
-    #Output('log-textarea', 'value'),
-    [Input('start-bot-button', 'n_clicks'),
-     Input('stop-bot-button', 'n_clicks')]
-)
 def run_trading_bot(start_bot_clicks, stop_bot_clicks, log_value):
     global trading_thread
     ctx = dash.callback_context
