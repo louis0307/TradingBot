f253332be66d24bcb625914c273e89c6b9d932d3
siegelou <siegelou@students.zhaw.ch>
2025-02-14 16:32:35 +0100

    v2.5-Trading

--- data/stream_data.py
+++ data/stream_data.py
@@ -6,10 +6,11 @@ import time
 from misc.logger_config import logger
 from config import stop_event
 from binance import ThreadedWebsocketManager
+from misc.login import test_api_key, test_secret_key
 import threading
 
 def stream_data():
-    twm = ThreadedWebsocketManager()
+    twm = ThreadedWebsocketManager(api_key=test_api_key, api_secret=test_secret_key)
     twm.start()
     for asset in ASSET_LIST:
         twm.start_kline_socket(callback=handle_socket_message, symbol=asset.lower(), interval='15m')
