98542068246eec3b480a422730a8e2fb943de90f
siegelou <siegelou@students.zhaw.ch>
2025-03-15 13:48:18 +0100

    v2.5-Trading

--- data/stream_data.py
+++ data/stream_data.py
@@ -8,6 +8,7 @@ from misc.login import test_api_key, test_secret_key
 import threading
 
 def stream_data():
+    logger.info("Starting streaming data cron job.")
     twm = ThreadedWebsocketManager(api_key=test_api_key, api_secret=test_secret_key)
     twm.start()
     for asset in ASSET_LIST:
@@ -15,10 +16,15 @@ def stream_data():
         time.sleep(3)
         twm.start_kline_socket(callback=handle_socket_message, symbol=asset.lower(), interval='1h')
     logger.info("Streaming data initialized.")
+    run_duration = 300  # 10 minutes in seconds
+    start_time = time.time()
+
     try:
-        time.sleep(1)
-        while not stop_event.is_set():
+        while time.time() - start_time < run_duration:
+            if stop_event.is_set():
+                break
             time.sleep(5)
     finally:
         twm.stop()
         logger.info("Streaming data stopped.")
+        logger.info("Streaming data cron job completed.")
