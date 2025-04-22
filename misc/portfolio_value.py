aaf264344ea327ab29773e4ee2a2942e0d9905c2
siegelou <siegelou@students.zhaw.ch>
2025-04-22 21:17:19 +0200

    v3.0.1-live

--- misc/portfolio_value.py
+++ misc/portfolio_value.py
@@ -7,72 +7,75 @@ import warnings
 from sqlalchemy import text
 
 from misc.logger_config import logger
-from config import stop_event
+from config import stop_event, ASSET_LIST
 
 
-def calc_pv(asset):
+def calc_pv():
     if stop_event.is_set():
         logger.info("Trading Bot is not running. No PV calculations.")
         return
-    interval = INTERVALS
-    pd.options.mode.chained_assignment = None  # default='warn'
-    warnings.filterwarnings("ignore", category=FutureWarning)
-    warnings.filterwarnings("ignore", category=RuntimeWarning)
+
     query = f'SELECT * FROM "public"."TRADES"'
-    trades = pd.read_sql(query, stream)
-    trades = trades[trades["symbol"] == asset].copy()
-    trades["order_timestamp"] = pd.to_datetime(trades["order_timestamp"])
-    trades = trades.sort_values("order_timestamp")
+    trades_all = pd.read_sql(query, stream)
 
-    win_loss = 0
-    wins_losses = []
-    position = 0
-    portfolio_values = []
+    for asset in ASSET_LIST:
+        interval = INTERVALS
+        pd.options.mode.chained_assignment = None  # default='warn'
+        warnings.filterwarnings("ignore", category=FutureWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        trades = trades_all[trades_all["symbol"] == asset].copy()
+        trades["order_timestamp"] = pd.to_datetime(trades["order_timestamp"])
+        trades = trades.sort_values("order_timestamp")
 
-    if trades.empty:
-        now = pd.Timestamp.now()
-        rounded = now.replace(minute=45, second=0, microsecond=0)
-        portfolio_values.append({
-            "timestamp": rounded,
-            "portfolio_value": position
-        })
-        return pd.DataFrame(portfolio_values)
+        win_loss = 0
+        wins_losses = []
+        position = 0
+        portfolio_values = []
 
-    for _, group in trades.groupby("symbol"):
-        prev_row = None  # Initialize the previous row for each asset
-
-        for i, row in group.iterrows():
-            # For the first row, we don't need to compare
-            if prev_row is not None:
-                if prev_row["signal"] == 0 and row["signal"] != 0:
-                    position += win_loss
-                else:
-                    win_loss = INVESTMENT_AMT / row["price"] * (row["price"] - prev_row["price"]) * prev_row["signal"]
-                    position += win_loss
-
-            wins_losses.append({
-                "symbol": asset,
-                "timestamp": row["order_timestamp"],
-                "win_loss": win_loss
-            })
-
-            # Append portfolio value for this transaction
+        if trades.empty:
+            now = pd.Timestamp.now()
+            rounded = now.replace(minute=45, second=0, microsecond=0)
             portfolio_values.append({
-                "symbol": asset,
-                "timestamp": row["order_timestamp"],
+                "timestamp": rounded,
                 "portfolio_value": position
             })
+            return pd.DataFrame(portfolio_values)
 
-            prev_row = row  # Update prev_row for the next iteration
+        for _, group in trades.groupby("symbol"):
+            prev_row = None  # Initialize the previous row for each asset
 
-            with stream.connect() as conn:
-                # Use parameterized query for safety
-                conn.execute(text('DELETE FROM public."WINS_LOSSES" WHERE "symbol" = :symbol'), {"symbol": asset})
-                conn.commit()  # Commit deletion
-            pd.DataFrame(wins_losses).to_sql('WINS_LOSSES', stream, if_exists='append', index=True)
+            for i, row in group.iterrows():
+                # For the first row, we don't need to compare
+                if prev_row is not None:
+                    if prev_row["signal"] == 0 and row["signal"] != 0:
+                        position += win_loss
+                    else:
+                        win_loss = INVESTMENT_AMT / row["price"] * (row["price"] - prev_row["price"]) * prev_row["signal"]
+                        position += win_loss
 
-            with stream.connect() as conn:
-                # Use parameterized query for safety
-                conn.execute(text('DELETE FROM public."PORTFOLIO_VALUES" WHERE "symbol" = :symbol'), {"symbol": asset})
-                conn.commit()  # Commit deletion
-            pd.DataFrame(portfolio_values).to_sql('PORTFOLIO_VALUES', stream, if_exists='append', index=True)
+                wins_losses.append({
+                    "symbol": asset,
+                    "timestamp": row["order_timestamp"],
+                    "win_loss": win_loss
+                })
+
+                # Append portfolio value for this transaction
+                portfolio_values.append({
+                    "symbol": asset,
+                    "timestamp": row["order_timestamp"],
+                    "portfolio_value": position
+                })
+
+                prev_row = row  # Update prev_row for the next iteration
+
+                with stream.connect() as conn:
+                    # Use parameterized query for safety
+                    conn.execute(text('DELETE FROM public."WINS_LOSSES" WHERE "symbol" = :symbol'), {"symbol": asset})
+                    conn.commit()  # Commit deletion
+                pd.DataFrame(wins_losses).to_sql('WINS_LOSSES', stream, if_exists='append', index=True)
+
+                with stream.connect() as conn:
+                    # Use parameterized query for safety
+                    conn.execute(text('DELETE FROM public."PORTFOLIO_VALUES" WHERE "symbol" = :symbol'), {"symbol": asset})
+                    conn.commit()  # Commit deletion
+                pd.DataFrame(portfolio_values).to_sql('PORTFOLIO_VALUES', stream, if_exists='append', index=True)
