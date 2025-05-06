f847d6dd6e76445d8a8b22eece1a5803a523fb4a
siegelou <siegelou@students.zhaw.ch>
2025-05-06 15:28:22 +0200

    v3.1.0-live

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,4 +1,4 @@
-from trading.indicators import calculate_rsi, kdj
+from trading.indicators import calculate_rsi, kdj, calculate_atr
 import numpy as np
 from misc.logger_config import logger
 
@@ -10,11 +10,12 @@ def dat_preprocess(dat_in):
     dat['vol_ma10'] = dat.volume.rolling(10).mean()
     dat['ema_50'] = dat.close.ewm(span=50, adjust=False).mean()
     dat['ema_200'] = dat.close.ewm(span=200, adjust=False).mean()
-    dat['rsi_14'] = calculate_rsi(dat.close, period=14)
+    dat['rsi_14'], dat['stoch_rsi'] = calculate_rsi(dat.close, period=14, stoch_period=14)
     dat['volatility'] = dat.log_returns.rolling(window=10).std()
     dat['MACD'] = dat.close.ewm(span=12, adjust=False).mean() - dat.close.ewm(span=26, adjust=False).mean()
     dat['MACD_Signal'] = dat['MACD'].ewm(span=9, adjust=False).mean()
     dat['MACD_Hist'] = dat['MACD'] - dat['MACD_Signal']
+    dat['ATR'] = calculate_atr(dat)
     dat = kdj(dat)
     #dat = dat.dropna(axis=0)
     ret = dat.replace([np.inf, -np.inf], 0, inplace=False)
