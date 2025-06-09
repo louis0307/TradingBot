f679d956cf0143e62f95d52f50045be36563708e
siegelou <siegelou@students.zhaw.ch>
2025-06-09 12:34:51 +0200

    v3.1.0-live

--- data/stream_data.py
+++ data/stream_data.py
@@ -23,12 +23,12 @@ def stream_data():
 
         logger.info("Streaming data initialized.")
 
-        # Run for 3 minutes
-        run_duration = 120
+        # Run for 1 minutes
+        run_duration = 60
         start_time = time.time()
 
         while time.time() - start_time < run_duration:
-            time.sleep(2)
+            time.sleep(1)
 
     except Exception as e:
         logger.error(f"Error in stream_data: {e}")
